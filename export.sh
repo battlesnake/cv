7dbab5964ff63b13dd70eb8215ad01fc643b61bd
Mark K Cowan <mark@battlesnake.co.uk>
2019-07-05 22:29:51 +0300

    export: generate html redirect for github pages

--- export.sh
+++ export.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
+
+declare -r out="publish/cv_$(date -uIdate)_$(git rev-parse HEAD).pdf"
 
 mkdir -p build/
 
 pdflatex -output-directory build/ cv.tex
 
-cp build/cv.pdf publish/"cv_$(date -uIdate)_$(git rev-parse HEAD).pdf"
+cp build/cv.pdf "$out"
+
+cat > index.html <<EOF
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Mark's CV - redirecting</title>
+		<meta http-equiv="refresh" content="1;url=$out">
+	</head>
+	<body>
+		<h1><a href="$out">Redirecting...</a></h1>
+	</body>
+</html>
+EOF
