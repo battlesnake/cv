e939a965a7551c6760e421d58bd17484fb442f1b
Mark K Cowan <mark@battlesnake.co.uk>
2019-07-06 00:09:38 +0300

    og:metadata added to HTML, and selfie

--- export.sh
+++ export.sh
@@ -19,6 +19,16 @@ cat > docs/index.html <<EOF
 		<meta charset="utf-8">
 		<title>Mark's CV - redirecting</title>
 		<meta http-equiv="refresh" content="1;url=$out">
+		<meta property="og:title" content="Mark K Cowan">
+		<meta property="og:description" content="Mark's CV as of $(date -uIdate)">
+		<meta property="og:image" content="me.jpg">
+		<meta property="og:url" content="https://battlesnake.github.io/cv/">
+		<meta property="og:type" content="article">
+		<meta property="og:locale" content="en_GB" />
+		<meta name="twitter:card" content="me.jpg">
+		<link rel="icon" type="image/png" href="me.jpg">
+		<link rel="apple-touch-icon" href="me.jpg">
+		<link rel="shortcut icon" type="image/png" href="me.jpg">
 	</head>
 	<body>
 		<h1><a href="$out">Redirecting...</a></h1>
